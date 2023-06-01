cd1949ca3d880c1f202a596026925b7e529bb3ff
Subramanya1702 <keshavas@oregonstate.edu>
2023-05-31 23:57:37 -0700

    File difference checker utility

---
+++ FileDifferenceChecker.java
@@ -0,0 +1,93 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Class to store the mismatched line number, corresponding lines from first and second file
+ */
+class Tuple {
+    private final int lineNumber;
+    private final String line1;
+    private final String line2;
+
+    public Tuple(final int lineNumber, final String line1, final String line2) {
+        this.lineNumber = lineNumber;
+        this.line1 = line1;
+        this.line2 = line2;
+    }
+
+    public int getLineNumber() {
+        return this.lineNumber;
+    }
+
+    public String getLine1() {
+        return this.line1;
+    }
+
+    public String getLine2() {
+        return this.line2;
+    }
+}
+
+public class FileDifferenceChecker {
+    private final String firstFileName;
+    private final String secondFileName;
+
+    public FileDifferenceChecker(final String firstFileName, final String secondFileName) {
+        this.firstFileName = firstFileName;
+        this.secondFileName = secondFileName;
+    }
+
+    /**
+     * Prints line number and mismatched lines from the two files
+     *
+     * @param tuple An object of class Tuple
+     */
+    private void printDifference(final Tuple tuple) {
+        System.out.println("Difference on Line: " + tuple.getLineNumber());
+        System.out.println("First File: " + tuple.getLine1());
+        System.out.println("Second File: " + tuple.getLine2());
+        System.out.println();
+    }
+
+    /**
+     * Compares the two files and prints the difference
+     */
+    public void compare() throws IOException {
+        try (final BufferedReader firstFileReader = new BufferedReader(new FileReader(this.firstFileName));
+             final BufferedReader secondFileReader = new BufferedReader(new FileReader(this.secondFileName))) {
+            String firstLine = firstFileReader.readLine();
+            String secondLine = secondFileReader.readLine();
+            int lineNumber = 1;
+
+            while (firstLine != null && secondLine != null) {
+                if (firstLine.length() != secondLine.length()) {
+                    printDifference(new Tuple(lineNumber, firstLine, secondLine));
+                } else {
+                    if (!firstLine.equals(secondLine)) {
+                        printDifference(new Tuple(lineNumber, firstLine, secondLine));
+                    }
+                }
+
+                firstLine = firstFileReader.readLine();
+                secondLine = secondFileReader.readLine();
+                lineNumber++;
+            }
+
+            if (firstLine != null) {
+                while (firstLine != null) {
+                    printDifference(new Tuple(lineNumber, firstLine, ""));
+                    firstLine = firstFileReader.readLine();
+                    lineNumber++;
+                }
+            } else if (secondLine != null) {
+                while (secondLine != null) {
+                    printDifference(new Tuple(lineNumber, "", secondLine));
+                    secondLine = secondFileReader.readLine();
+                    lineNumber++;
+                }
+            }
+        }
+    }
+
+}
